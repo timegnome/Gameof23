5d1237c82f2abe24b65671ad38f55642f2e44cf8
TimeGnome <timegnome7@gmail.com>
2021-03-03 18:17:50 -0800

    Debugging card selection fixed out of bound card selection

--- app23.js
+++ app23.js
@@ -4,11 +4,10 @@ for (x = 2; x < 11 ; x++){
         deck.push(x);
     }
 }
-royals= ['J','J','J','J','Q','Q','Q','Q','K','K','K','K','A','A','A','A'];
+var royals= ['J','J','J','J','Q','Q','Q','Q','K','K','K','K','A','A','A','A'];
 royals.forEach(value => deck.push(value));
 print(deck);
-operations = ['+','-','*','&#xF7'];
-
+var operations = ['+','-','*','&#xF7'];
 function shuffle(array) {
     var m = array.length, t, i;
   
@@ -42,23 +41,14 @@ function startTimer(duration, display, numply = 2) {
       deckpos = [];
       for(i =0 ; i <numply; i++)
       {
-        deckpos.push(i*Math.ceil(52/numply)+Math.ceil(52/numply));
+        deckpos.push(i*Math.ceil(52/numply)+Math.ceil(52/numply)-1);
       }
       deckpos.push(51);
       total = 0;
       deck = shuffle(deck);
       var answer = d3.select("#sortable");
-      answer.html('');
-      for ( i = 0; i < deckpos.length-1; i ++)
-      {
-        for(y = 0; y< 4/numply; y++)
-        {
-          li = answer.append('li').attr('class','ui-state-default');
-          li.append('text').html(deck[deckpos[i]]);
-          deck.push(deck.splice(deckpos[i],1)[0]);
-          deckpos[i]--;
-        }
-      }
+      
+      addCards(answer.selectAll('li'), deck, deckpos, numply);
       // The amount of time left before a new round and results are shown
       setInterval(function () {
         minutes = parseInt(timer / 60, 10);
@@ -73,51 +63,76 @@ function startTimer(duration, display, numply = 2) {
           answer = d3.select("#sortable");
           if (timer == -10) {
               timer = duration;
-              answer.html('');
-              // pull cards from amount of players into 4
-              for ( i = 0; i < deckpos.length-1; i ++)
-              {
-                for(y = 0; y< Math.ceil(4/numply); y++)
-                {
-                  li = answer.append('li').attr('class','ui-state-default');
-                  li.append('text').html(deck[deckpos[i]]);
-                  deck.push(deck.splice(deckpos[i],1)[0]);
-                  deckpos[i]--;
-                  
-                }
-              }
+              // pull 4 cards from amount of players
+              addCards(answer.selectAll('li'),deck, deckpos, numply);
           }
           else{
-            var orderIndex = 0;
-            order = answer.selectAll('li').nodes()
-
+            order = answer.selectAll('li').nodes();
             opers = d3.selectAll('#oper').nodes();
-            opers.forEach(val => {
-              switch(val){
-                case '+':
-                  total += order[orderIndex].textContent;
-                  orderIndex++;
-                  break;
-                case '-':
-                  total -= order[orderIndex].textContent;
-                  orderIndex++;
-                  break
-                case '*':
-                  total *= order[orderIndex].textContent;
-                  orderIndex++;
-                  break;
-                case '&#xF7':
-                  total /= order[orderIndex].textContent;
-                  orderIndex++;
-                  break;
-              }
-            });
-            console.log(total)
+            d3.select('#total').html(calctotal(order, opers));
           }
         }
       }, 1000);
 
   })   
+}
+
+function addCards(cardlist, Deck, deckPos, numPly)
+{
+  var content = []
+  for ( i = 0; i < deckPos.length-1; i ++)
+  {
+    for(y = 0; y< Math.ceil(4/numPly); y++)
+    {
+      content.push(Deck[deckPos[i]])
+      Deck.push(Deck.splice(deckPos[i],1)[0]);
+      deckPos[i]--;
+    }
+  }
+  cardlist.data(content)
+  .each(function(d,i){
+    if(d === 'A'){
+      this.on('dblclick', function(d){
+      this.html(`${12-this.html()}`)})}
+    d.append('text')
+      .html(1);
+  })
+}
+function calctotal(numOrder, totalOpers)
+{
+  console.log(totalOpers)
+  var orderIndex = 0, temp = 0;
+  totalOpers.forEach(operVal => {
+    var val = numOrder[orderIndex].textContent;
+    if(val in ['K','Q','J']){
+        val= 10;}
+    // if(val === 'A-1' || val === 'A'){
+    //   val = 1
+    // }
+    // if(val === 'A-11'){
+    //   val = 11
+    // } 
+    switch(operVal){
+      case '+':
+        temp += val;
+        orderIndex++;
+        break;
+      case '-':
+        temp -= val;
+        orderIndex++;
+        break
+      case '*':
+        temp *= val;
+        orderIndex++;
+        break;
+      case '&#xF7':
+        temp /= val;
+        orderIndex++;
+        break;
+    }
+  });
+  console.log(temp)
+  return temp;
 }
 var fiveMinutes = 60 * 2,
 display = document.querySelector('#time');
